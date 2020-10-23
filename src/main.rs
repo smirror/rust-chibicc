b413e2c26888e9fecdeb516f052f3193cfd4a36c
ALinWndr <sordcord@gmail.com>
2020-10-23 11:41:11 +0900

    Compile an integer to an exectuable that exits with the given number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let arg = std::env::args().nth(1).unwrap();
+    
+    let p = {
+        usize::from_str_radix(arg.as_str(), 10).unwrap()
+    };
+    // The first token must be a number
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+
+    // ... followed by either `+ <number>` or `- <number>`.
+    println!("main:");
+    println!("   mov rax, {}", p);
+    while (p)
+    println!("   ret");
 }
