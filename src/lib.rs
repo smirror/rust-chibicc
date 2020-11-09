43f4ab43512a899d964fb07bd6bc21baa3fb87eb
ALinWndr <23025996+smirror@users.noreply.github.com>
2020-11-09 18:50:13 +0900

    step3:replace space' '

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,1 @@
-pub fn split_digit(s: &str) -> (&str, &str) {
-    let first_non_num = s.find(|c| !char::is_numeric(c)).unwrap_or(s.len());
-    s.split_at(first_non_num)
-}
+
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::env;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let arg = args[1].to_string() + " ";
+    let mut arg = args[1].to_string();
+    arg.retain(|c| c != ' ');
+    arg += " ";
     // The first token must be a number
     println!(".intel_syntax noprefix");
     println!(".global main");
 
     // ... followed by either `+ <number>` or `- <number>`.
     println!("main:");
-    let mut n: String = "".to_string();
+    let mut n = "".to_string();
     let mut operator = '\0';
     for p in arg.as_str().chars() {
-        if !p.is_ascii_digit(){
+        if !p.is_ascii_digit() {
             match operator {
-                '\0'  => {
+                '\0' => {
                     println!("   mov rax, {}", n);
                     n = "".to_string();
                 }
@@ -32,7 +33,6 @@ fn main() {
             operator = p;
         } else {
             n.push(p);
-            continue;
         }
     }
     println!("   ret");
